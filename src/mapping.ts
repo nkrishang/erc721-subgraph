92890eaf29d6b3db48db3e4d8c2b7f204d59e6dd
nkrishang <kn1457@nyu.edu>
2021-08-11 14:12:31 -0400

    builds successfully; fails on deploy

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,87 +1,35 @@
-import { BigInt } from "@graphprotocol/graph-ts"
-import {
-  Apes,
-  Approval,
-  ApprovalForAll,
-  Paused,
-  RoleAdminChanged,
-  RoleGranted,
-  RoleRevoked,
-  Transfer,
-  Unpaused
-} from "../generated/Apes/Apes"
-import { ExampleEntity } from "../generated/schema"
+import { Address } from "@graphprotocol/graph-ts"
+import {CoolCats, Transfer} from "../generated/CoolCats/CoolCats"
+import { Account, NFT } from "../generated/schema" 
 
-export function handleApproval(event: Approval): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+/**
+ * 
+ * @param event Transfer(address from, address to, uint256 tokenId)
+ */
+export function handleTransfer(event: Transfer): void {
+  let nftAddress: Address = Address.fromString("0xB467a2b26E61fAd4D8B043B79E24c6d4e6Fa5a71")
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (entity == null) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
+  // Create receiver account if it doesn't already exist.
+  let recevierAccountId = event.params.to.toHexString()
+  let receiverAccount = Account.load(recevierAccountId)
 
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
+  if(receiverAccount == null) {
+    receiverAccount = new Account(recevierAccountId)
+    receiverAccount.address = event.params.from
   }
 
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
+  // Create `NFT` if it is being minted.
+  let nftId = event.params.tokenId.toString()
+  let nft = NFT.load(nftId)
+  if(nft == null) {
+    nft = new NFT(nftId)
+    nft.tokenId = event.params.tokenId
 
-  // Entity fields can be set based on event parameters
-  entity.owner = event.params.owner
-  entity.approved = event.params.approved
+    let nftContract = CoolCats.bind(nftAddress)
+    nft.metadataURI = nftContract.tokenURI(event.params.tokenId)
+  }
 
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.DEFAULT_ADMIN_ROLE(...)
-  // - contract.MINTER_ROLE(...)
-  // - contract.PAUSER_ROLE(...)
-  // - contract.balanceOf(...)
-  // - contract.getApproved(...)
-  // - contract.getRoleAdmin(...)
-  // - contract.getRoleMember(...)
-  // - contract.getRoleMemberCount(...)
-  // - contract.hasRole(...)
-  // - contract.isApprovedForAll(...)
-  // - contract.name(...)
-  // - contract.ownerOf(...)
-  // - contract.paused(...)
-  // - contract.supportsInterface(...)
-  // - contract.symbol(...)
-  // - contract.tokenByIndex(...)
-  // - contract.tokenOfOwnerByIndex(...)
-  // - contract.tokenURI(...)
-  // - contract.totalSupply(...)
+  nft.owner = recevierAccountId
+  nft.save()
 }
 
-export function handleApprovalForAll(event: ApprovalForAll): void {}
-
-export function handlePaused(event: Paused): void {}
-
-export function handleRoleAdminChanged(event: RoleAdminChanged): void {}
-
-export function handleRoleGranted(event: RoleGranted): void {}
-
-export function handleRoleRevoked(event: RoleRevoked): void {}
-
-export function handleTransfer(event: Transfer): void {}
-
-export function handleUnpaused(event: Unpaused): void {}
